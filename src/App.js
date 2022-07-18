1f440d3ab37fd2580ef891a15e7eb23234e56a5a
tiliax <97850682+tiliax@users.noreply.github.com>
2022-07-18 15:22:12 +0200

    checkbox

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ const App = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
 
-  const questions = [...htmlandcss, ...jsandreact];
-  // const log = () => {
-  //   console.log(htmlandcss);
-  //   console.log(jsandreact);
-  //   console.log(questions);
-  // };
-  // log();
+  const [questions, setQuestions] = useState([...htmlandcss, ...jsandreact]);
+  // const [questionshtml, setQuestionshtml] = useState([...htmlandcss])
+  // const [questionsjs, setQuestionsjs] = useState([...jsandreact])
+  const [html, setHtml] = useState(true)
+  const [react, setReact] = useState(true)
 
   const optionClicked = (isCorrect) => {
     // Increment the score
@@ -33,10 +31,36 @@ const App = () => {
     setCurrentQuestion(0);
     setShowResults(false);
   };
+ const handleChange = (e) => {
+  // if ('htmlandcss' === e.target.name) {
+  //   setQuestionshtml(prev => [...prev, e.target.name])
+  // } else if (jsandreact === e.target.name) {
+  //   setQuestionsjs(prev => [...prev, e.target.name])
+  // } else if (htmlandcss === e.target.name && jsandreact === e.target.name){
+  //   setQuestions(prev => [...prev, e.target.name])
+  // } 
+  setReact(true)
+ }
+
   return (
     <div className="App">
       {/* 1. Header */}
       <h1>Web Dev Quizz</h1>
+
+      {/* Checkbox */}
+      <div>
+        <label>
+        <input type="checkbox" name='htmlandcss' id='htmlandcss'  onChange={handleChange}/>HTML und CSS
+        </label>
+      </div>
+      
+      <div>
+        <label>
+        <input type="checkbox" name='jsandreact' id='jsandreact'  onChange={handleChange}/>JS und React
+        </label>
+      </div>
+
+
 
       {/* 2. Aktuelle Punktzahl */}
       <h2>Aktuelle Punktzahl: {score}</h2>
