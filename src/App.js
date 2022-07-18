3815b5c703b4d635456c7d03c0bae390cf845c79
tiliax <97850682+tiliax@users.noreply.github.com>
2022-07-18 12:02:24 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>For Linde</h1>
+    <div className='App'>
+      {/* 1. Header */}
+      <h1>Web Dev Quizz</h1>
+
+      {/* 2. Aktuelle Punktzahl */}
+      <h2>Aktuelle Punktzahl: 8</h2>
+
+      {/* 3. Fragen Karte */}
+      <div className="fragenKarte">
+        <h2>Frage 1 von 10</h2>
+        <h3 className='fragenText'>Wie viele Leute sind in Gruppe Vier?</h3>
+
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
+
+      {/* 4. Endergebnis */}
+      <div className='endergebnis'>
+        <h1>Endergebnis</h1>
+        <h2>
+          1 von 10 korrekt</h2>
+        <button>Neustart</button>
+
+      </div>
+
     </div>
   );
 }
